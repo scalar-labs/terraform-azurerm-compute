2d317806902a5f14d912beb2d6ddbd28c14735cd
tei-k <tei@leosys.space>
2021-02-24 13:48:35 +0900

    Upgrade terraform to 0.14 (#8)

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "vm_ids" {
   description = "Virtual machine ids created."
-  value       = "${concat(azurerm_virtual_machine.vm-windows.*.id, azurerm_virtual_machine.vm-windows-with-datadisk.*.id, azurerm_virtual_machine.vm-linux.*.id, azurerm_virtual_machine.vm-linux-with-datadisk.*.id)}"
+  value       = concat(azurerm_virtual_machine.vm-windows.*.id, azurerm_virtual_machine.vm-windows-with-datadisk.*.id, azurerm_virtual_machine.vm-linux.*.id, azurerm_virtual_machine.vm-linux-with-datadisk.*.id)
 }
 
 output "vm_identities" {
   description = "identities of virtual machines created."
-  value       = "${concat(azurerm_virtual_machine.vm-windows.*.identity, azurerm_virtual_machine.vm-windows-with-datadisk.*.identity, azurerm_virtual_machine.vm-linux.*.identity, azurerm_virtual_machine.vm-linux-with-datadisk.*.identity)}"
+  value       = concat(azurerm_virtual_machine.vm-windows.*.identity, azurerm_virtual_machine.vm-windows-with-datadisk.*.identity, azurerm_virtual_machine.vm-linux.*.identity, azurerm_virtual_machine.vm-linux-with-datadisk.*.identity)
 }
 
 output "network_security_group_id" {
@@ -20,27 +20,27 @@ output "network_security_group_name" {
 
 output "network_interface_ids" {
   description = "ids of the vm nics provisoned."
-  value       = "${azurerm_network_interface.vm.*.id}"
+  value       = azurerm_network_interface.vm.*.id
 }
 
 output "network_interface_private_ip" {
   description = "private ip addresses of the vm nics"
-  value       = "${azurerm_network_interface.vm.*.private_ip_address}"
+  value       = azurerm_network_interface.vm.*.private_ip_address
 }
 
 output "public_ip_id" {
   description = "id of the public ip address provisoned."
-  value       = "${azurerm_public_ip.vm.*.id}"
+  value       = azurerm_public_ip.vm.*.id
 }
 
 output "public_ip_address" {
   description = "The actual ip address allocated for the resource."
-  value       = "${azurerm_public_ip.vm.*.ip_address}"
+  value       = azurerm_public_ip.vm.*.ip_address
 }
 
 output "public_ip_dns_name" {
   description = "fqdn to connect to the first vm provisioned."
-  value       = "${azurerm_public_ip.vm.*.fqdn}"
+  value       = azurerm_public_ip.vm.*.fqdn
 }
 
 output "availability_set_id" {
