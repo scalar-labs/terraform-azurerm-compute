44e443e04f931d452d95b37348d0a98db56722ae
Jishnu.j <jishnu.j@innovaturelabs.org>
2023-04-04 10:45:35 +0530

    os set to 20.04 with gen 1

--- os/variables.tf
+++ os/variables.tf
@@ -5,7 +5,7 @@ variable "vm_os_simple" {
 # Definition of the standard OS with "SimpleName" = "publisher,offer,sku"
 variable "standard_os" {
   default = {
-    "UbuntuServer"  = "Canonical,UbuntuServer,16.04-LTS"
+    "UbuntuServer"  = "Canonical,0001-com-ubuntu-server-focal,20_04-lts"
     "WindowsServer" = "MicrosoftWindowsServer,WindowsServer,2016-Datacenter"
     "RHEL"          = "RedHat,RHEL,7.5"
     "openSUSE-Leap" = "SUSE,openSUSE-Leap,42.2"
